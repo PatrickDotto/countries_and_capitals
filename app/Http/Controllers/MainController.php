32b428edbb1af909344923d1fdd7fbac65388afd
Patrick Dotto <patrickdotto05@gmail.com>
2026-02-17 15:20:15 -0300

    Tela inicial feita

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
@@ -13,8 +14,34 @@ class MainController extends Controller
         $this->app_data = require(app_path('app_data.php'));
     }
 
-    public function show_data()
+    public function startGame(): View
     {
-        return response()->json($this->app_data);
+        return view('home');
+    }
+
+    public function prepareGame(Request $request)
+    {
+        $request->validate(
+        [
+            'total_questions' => 'required|integer|min:3|max:30'
+        ],
+        [
+            'total_questions.required' => 'O número de questões é obrigatório',
+            'total_questions.integer' => 'O número de questões tem que ser um valor inteiro',
+            'total_questions.min' => 'O número de questões tem que ser até :min questões',
+            'total_questions.max' => 'O número de questões tem que ser até :max questões',
+        ]
+        
+        );
+        $total_questions = intval($request->input('total_questions'));
+    
+        $quiz = $this->prepareQuiz($total-questions);
+
+        dd($quiz);
+    }
+
+    private function prepareQuiz($total_questions)
+    {
+
     }
 }
